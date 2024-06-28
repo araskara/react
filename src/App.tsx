8e92e70be9fb15f0b35d7b6a2b1241be4fa78382
araskara <arazgare1@gmail.com>
2024-06-28 19:19:22 +0200

    Adding Allert  in React-Typescrip

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,35 @@
 import React from 'react';
 import ListGroup from './components/ListGroup';
+import Alert from './components/Alert';
 
-function App() {
+
+const App = () => {
+  return (
+    <div>
+      <Alert>
+        Hello <span>World</span>
+      </Alert>
+    </div>
+  );
+}
+
+export default App
+
+
+
+
+
+
+
+
+
+
+
+
+
+//---------Adding List-------------------
+
+/*function App() {
 
   let items= ["New York", "Paris", "Istanbul","Izmir", "Vienna", "Salzburg", "Berlin", "Munich"]
   const handleSelectItem = (item: string) =>{console.log(item)}
@@ -13,3 +41,5 @@ function App() {
 }
 
 export default App;
+
+*/
