31f7a59017ff48cfd68307cfe507ac22504b6ac6
araskara <arazgare1@gmail.com>
2024-06-28 19:49:15 +0200

    Button Component  in React-Typescrip

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import ListGroup from './components/ListGroup';
 import Alert from './components/Alert';
+import Button from './components/Button';
 
 
 const App = () => {
   return (
     <div>
+      <Button color='danger' onClick={()=> console.log('Clicked!')}>My Button</Button>
       <Alert>
         Hello <span>World</span>
       </Alert>
