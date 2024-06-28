4b01db6e093229a9e8860afe64146fca2f1b2d06
araskara <arazgare1@gmail.com>
2024-06-28 20:10:55 +0200

    Adding Showing Alert  in React-Typescrip

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ListGroup from './components/ListGroup';
 import Alert from './components/Alert';
 import Button from './components/Button';
+ 
 
 
-const App = () => {
-  return (
-    <div>
-      <Button color='danger' onClick={()=> console.log('Clicked!')}>My Button</Button>
-      <Alert>
-        Hello <span>World</span>
-      </Alert>
-    </div>
-  );
-}
-
-export default App
-
-
-
-
-
-
-
-
-
-
-
-
-
-//---------Adding List-------------------
-
-/*function App() {
+function App () {
 
   let items= ["New York", "Paris", "Istanbul","Izmir", "Vienna", "Salzburg", "Berlin", "Munich"]
   const handleSelectItem = (item: string) =>{console.log(item)}
+
+  const [alertVisible, setAlertVisibility] = useState(false);
+
   return (
     <div>
+      <Button color='danger' onClick={()=> setAlertVisibility(true)}>My Button</Button>
+      {alertVisible&&<Alert onClose={()=> setAlertVisibility(false)}> My Alert</Alert>}
       <ListGroup items={items} heading={"Cities"} onSelectItem={handleSelectItem}/>
     </div>
   );
 }
 
-export default App;
-
-*/
+export default App
