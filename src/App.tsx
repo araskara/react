b57e772a4938caa5085d8ca59b0d07633782c943
araskara <arazgare1@gmail.com>
2024-07-06 00:58:00 +0200

    Adding Categories to Expence App

--- src/App.tsx
+++ src/App.tsx
@@ -4,53 +4,56 @@ import Alert from './components/Alert';
 import Button from './components/Button';
 import ExpenceList from './expense-tracker/components/ExpenceList';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import ExpenseFilter from './expense-tracker/components/ExpenceFilter';
+import categories from './expense-tracker/categories';
 
- 
+function App() {
+  const [expenses, setExpenses] = useState([
+    { id: 1, description: 'aaa', amount: 10, category: 'Utilities' },
+    { id: 2, description: 'aaa', amount: 10, category: 'Utilities' },
+    { id: 3, description: 'aaa', amount: 10, category: 'Utilities' },
+    { id: 4, description: 'aaa', amount: 10, category: 'Utilities' },
+  ]);
 
+  const [selectedCategory, setSelectedCategory] = useState('');
 
-function App () {
+  const filteredExpenses = selectedCategory
+    ? expenses.filter(expense => expense.category === selectedCategory)
+    : expenses;
 
-
-
-  const [expences, setExpences] = 
-  useState([
-    
-    {id: 1, description: 'aaa', amount: 10, category: 'Utilities' },
-    {id: 2, description: 'aaa', amount: 10, category: 'Utilities'},
-    {id: 3, description: 'aaa', amount: 10, category: 'Utilities'},
-    {id: 4, description: 'aaa', amount: 10, category: 'Utilities'},
-
-  ])
-
-
-  let items= ["New York", "Paris", "Istanbul","Izmir", "Vienna", "Salzburg", "Berlin", "Munich"]
-  const handleSelectItem = (item: string) =>{console.log(item)}
+  let items = ["New York", "Paris", "Istanbul", "Izmir", "Vienna", "Salzburg", "Berlin", "Munich"];
+  const handleSelectItem = (item: string) => { console.log(item) }
 
   const [alertVisible, setAlertVisibility] = useState(false);
 
   return (
     <Router>
-        <div>
-            <nav>
-                <ul>
-                    <li><Link to="/button-alert">Button and Alert</Link></li>
-                    <li><Link to="/listgroup">List Group</Link></li>
-                    <li><Link to="/expenselist">Expense List</Link></li>
-                </ul>
-            </nav>
-            <Routes>
-                <Route path="/button-alert" element={
-                    <>
-                        <Button color='danger' onClick={() => setAlertVisibility(true)}>My Button</Button>
-                        {alertVisible && <Alert onClose={() => setAlertVisibility(false)}>My Alert</Alert>}
-                    </>
-                } />
-                <Route path="/listgroup" element={<ListGroup items={items} heading={"Cities"} onSelectItem={handleSelectItem} />} />
-                <Route path="/expenselist" element={<ExpenceList expenses={expences} onDelete={(id) => setExpences(expences.filter((e) => e.id !== id))} />} />
-            </Routes>
-        </div>
+      <div>
+        <nav>
+          <ul>
+            <li><Link to="/button-alert">Button and Alert</Link></li>
+            <li><Link to="/listgroup">List Group</Link></li>
+            <li><Link to="/expenselist">Expense List</Link></li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/button-alert" element={
+            <>
+              <Button color='danger' onClick={() => setAlertVisibility(true)}>My Button</Button>
+              {alertVisible && <Alert onClose={() => setAlertVisibility(false)}>My Alert</Alert>}
+            </>
+          } />
+          <Route path="/listgroup" element={<ListGroup items={items} heading={"Cities"} onSelectItem={handleSelectItem} />} />
+          <Route path="/expenselist" element={
+            <>
+              <ExpenseFilter onSelectCategory={category => setSelectedCategory(category)} />
+              <ExpenceList expenses={filteredExpenses} onDelete={(id) => setExpenses(expenses.filter((e) => e.id !== id))} />
+            </>
+          } />
+        </Routes>
+      </div>
     </Router>
-);
+  );
 }
 
 export default App;
